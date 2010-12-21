17149a7261ab3327666f52abd2b19dae482e6b47
Tony McCrae <tony@Morden.local>
2010-12-21 23:03:50 +0000

    First cut of inserting placeholder articles for missing articles (ie. take downs etc).

--- src/main/java/nz/gen/wellington/guardian/contentapiproxy/datasources/SavedContentDataSource.java
+++ src/main/java/nz/gen/wellington/guardian/contentapiproxy/datasources/SavedContentDataSource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import nz.gen.wellington.guardian.contentapiproxy.model.Article;
 
 import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
 
 import com.google.inject.Inject;
 
@@ -23,9 +24,23 @@ public class SavedContentDataSource {
 	public List<Article> getArticles(List<String> articleIds) {		
 		List<Article> savedArticles = new ArrayList<Article>();		
 		for (String articleId : articleIds) {
-			savedArticles.add(fetchArticle(articleId));
+			Article article = fetchArticle(articleId);
+			if (article != null) {
+				savedArticles.add(article);
+			} else {
+				savedArticles.add(missingArticlePlaceholderFor(articleId));
+			}
 		}
 		return savedArticles;
+	}
+
+	private Article missingArticlePlaceholderFor(String articleId) {
+		Article missingArticle = new Article();
+		missingArticle.setId(articleId);
+		missingArticle.setHeadline("Missing article");
+		missingArticle.setPubDate(new DateTime().toDateTime());
+		missingArticle.setStandfirst("This article could not be retrieved. It may no longer be available. (" + articleId + ")");
+		return missingArticle;
 	}
 
 	private Article fetchArticle(String contentId) {
