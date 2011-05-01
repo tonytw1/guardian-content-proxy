f96e15f8684061a3dd743ca40a53f947b8d29e7c
Tony McCrae <tony@Morden.local>
2011-05-01 14:02:13 +0100

    Test coverage on existing article thumbnail field functionality.

--- src/test/java/nz/gen/wellington/guardian/contentapiproxy/datasources/rss/RssEntryToArticleConvertorTest.java
+++ src/test/java/nz/gen/wellington/guardian/contentapiproxy/datasources/rss/RssEntryToArticleConvertorTest.java
@@ -1,5 +1,8 @@
 package nz.gen.wellington.guardian.contentapiproxy.datasources.rss;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -9,24 +12,25 @@ import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
-import junit.framework.TestCase;
 import nz.gen.wellington.guardian.contentapi.cleaning.HtmlCleaner;
 import nz.gen.wellington.guardian.model.Article;
 import nz.gen.wellington.guardian.model.Section;
+
+import org.junit.Before;
+import org.junit.Test;
 
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.SyndFeedInput;
 
-public class RssEntryToArticleConvertorTest extends TestCase {
+public class RssEntryToArticleConvertorTest {
 	
 	private SyndFeed feed;
 	private RssEntryToArticleConvertor convertor;
 	private Map<String, Section> sections;
 	
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
+	@Before
+	public void setUp() throws Exception {
 		String content = loadContent("rss.htm").toString();
 		
 		SyndFeedInput input = new SyndFeedInput();
@@ -36,28 +40,34 @@ public class RssEntryToArticleConvertorTest extends TestCase {
 		sections.put("poltics", new Section("politics", "Politics"));
 	}
 	
-	public void testShouldExtractAllFieldsCorrectly() throws Exception {
-		SyndEntry firstEntry = (SyndEntry) feed.getEntries().get(0);
-		
+	@Test
+	public void shouldExtractAllFieldsCorrectly() throws Exception {
+		SyndEntry firstEntry = (SyndEntry) feed.getEntries().get(0);		
 		Article article = convertor.entryToArticle(firstEntry, sections);
+		
 		assertEquals("Cameron defends Osborne's budget 'to protect the poor'", article.getHeadline());
 		assertEquals("politics", article.getSection().getId());
 		assertEquals("Andrew Sparrow", article.getByline());
-		
-		assertEquals(7, article.getTags().size());
-				
 		assertEquals("All the news from Westminster including minute-by-minute coverage of PMQs and all the latest reaction to the budget\n\nRead a summary of events so far", article.getStandfirst());
+		assertEquals(7, article.getTags().size());				
 		// TODO This assert suffers from an BST problem which would be nice to work out an answer to.
 		//assertEquals(new DateTime(2010, 6, 23, 7, 3, 39, 0), article.getPubDate());	
 	}
 
+	@Test
+	public void articleThumbnailUrlShouldBeSetToFirstThumbnailSizedMediaElementsUrl() throws Exception {
+		SyndEntry firstEntry = (SyndEntry) feed.getEntries().get(0);		
+		Article article = convertor.entryToArticle(firstEntry, sections);	
+		assertEquals("http://static.guim.co.uk/sys-images/Politics/Pix/pictures/2010/6/22/1277210802573/Budget-2010-George-Osborn-002.jpg", article.getThumbnail());
+	}
 	
+	@Test
 	public void testShouldIgnoreNonArticles() throws Exception {
 		SyndEntry galleryEntry = (SyndEntry) feed.getEntries().get(11);
 		assertNull(convertor.entryToArticle(galleryEntry, sections));
 	}
-	
-	protected StringBuffer loadContent(String filename) throws IOException {
+		
+	private StringBuffer loadContent(String filename) throws IOException {
         StringBuffer content = new StringBuffer();
         File contentFile = new File(ClassLoader.getSystemClassLoader().getResource(filename).getFile());
         Reader freader = new FileReader(contentFile);
@@ -70,6 +80,6 @@ public class RssEntryToArticleConvertorTest extends TestCase {
         in.close();
         freader.close();
         return content;
-}
+	}
 
 }
