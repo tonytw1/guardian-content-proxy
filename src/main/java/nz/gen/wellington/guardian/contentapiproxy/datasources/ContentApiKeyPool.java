c15766925ee5eea0b90f8f10ef520c33d21edeb8
Tony McCrae <tony@eelpieconsulting.co.uk>
2011-10-23 13:13:32 +0100

    Move apikey to properties file.

--- src/main/java/nz/gen/wellington/guardian/contentapiproxy/datasources/ContentApiKeyPool.java
+++ src/main/java/nz/gen/wellington/guardian/contentapiproxy/datasources/ContentApiKeyPool.java
@@ -5,24 +5,26 @@ import nz.gen.wellington.guardian.contentapiproxy.caching.Cache;
 import org.apache.log4j.Logger;
 
 import com.google.inject.Inject;
+import com.google.inject.name.Named;
 
 public class ContentApiKeyPool {
 	
 	private final Logger log = Logger.getLogger(ContentApiKeyPool.class);
 	
 	private static final String CACHE_KEY_PREFIX = "expired-api-key:";
-	public static final String API_KEY = "3qyjrth68tdjqqh77sdc7ke8";	// TODO push to a property
 	private static final int DEFAULT_TTL = 60 * 60;
 	
 	private Cache cache;
+	private String apiKey;
 	
 	@Inject
-	public ContentApiKeyPool(Cache cache) {
+	public ContentApiKeyPool(Cache cache, @Named("apiKey") String apiKey) {
 		this.cache = cache;
+		this.apiKey = apiKey;
 	}
 	
-	public String getAvailableApiKey() {		
-		final String availableApiKey = API_KEY;
+	public String getAvailableApiKey() {	
+		final String availableApiKey = apiKey;
 		if (cache.get(CACHE_KEY_PREFIX + availableApiKey) != null) {
 			log.info("Api key is marked as over rate; returning null: " + availableApiKey);
 			return null;
--- src/main/java/nz/gen/wellington/guardian/contentapiproxy/servlets/GuiceServletConfig.java
+++ src/main/java/nz/gen/wellington/guardian/contentapiproxy/servlets/GuiceServletConfig.java
@@ -1,25 +1,58 @@
 package nz.gen.wellington.guardian.contentapiproxy.servlets;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.name.Names;
 import com.google.inject.servlet.GuiceServletContextListener;
 import com.google.inject.servlet.ServletModule;
 
 public class GuiceServletConfig extends GuiceServletContextListener {
 	
+	private static Logger log = Logger.getLogger(GuiceServletConfig.class);
+
+	protected static final String PROPERTIES_FILE = "guardian-content-proxy.properties";
+
 	@Override
 	protected Injector getInjector() {
 		return Guice.createInjector(new ServletModule() {
 
-		     @Override
-		     protected void configureServlets() {
-		    	 serve("/about").with(AboutProxyServlet.class);
-		    	 serve("/saved").with(SavedProxyServlet.class);
-		    	 serve("/search").with(SearchProxyServlet.class);
-		    	 serve("/sections").with(SectionProxyServlet.class);
-		    	 serve("/tags").with(TagsProxyServlet.class);
-		     }
-		     
+			@Override
+			protected void configureServlets() {
+				try {
+					bindProperties();
+
+					serve("/about").with(AboutProxyServlet.class);
+					serve("/saved").with(SavedProxyServlet.class);
+					serve("/search").with(SearchProxyServlet.class);
+					serve("/sections").with(SectionProxyServlet.class);
+					serve("/tags").with(TagsProxyServlet.class);
+
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+
+			private void bindProperties() throws FileNotFoundException, IOException {
+				URL propertiesResource = this.getClass().getClassLoader().getResource(PROPERTIES_FILE);
+				log.info("Properties file url is: " + propertiesResource.toExternalForm());
+				Properties properties = new Properties();
+				properties.load(new FileInputStream(propertiesResource.getFile()));
+				Names.bindProperties(binder(), properties);
+			}
+			
 		});
 	}
 
